df15f991beab8c0980a06bef1e1bfd5e3d4b2045
kricsleo <kricsleo@163.com>
2025-03-05 19:04:11 +0800

    update

--- src/icon-theme/definitions.ts
+++ src/icon-theme/definitions.ts
@@ -1,4 +1,4 @@
-import type { IconDefinition } from "./types";
+import type { IconDefinition } from './types';
 
 export const iconDefinitions = {
   file: {
@@ -10,10 +10,7 @@ export const iconDefinitions = {
   folderExpanded: {
     iconPath: './icons/folder-expanded.svg'
   },
-  config: {
-    names: ['tsconfig.json'],
-    "iconPath": "./icons/config.svg" 
-  },
+
   audio: {
     extensions: ['mp3', 'wav', 'ogg'],
     iconPath: './icons/audio.svg'
@@ -35,49 +32,6 @@ export const iconDefinitions = {
     iconPath: './icons/font.svg'
   },
 
-  javascript: {
-    extensions: ['js', 'mjs', 'cjs'],
-    iconPath: './icons/javascript.svg'
-  },
-  'javascript-test': {
-    extensions: ['test.js', 'test.mjs', 'test.cjs', 'spec.js', 'spec.mjs', 'spec.cjs'],
-    iconPath: './icons/javascript-test.svg'
-  },
-  'javascript-config': {
-    extensions: ['config.js', 'config.mjs', 'config.cjs'],
-    iconPath: './icons/javascript-config.svg'
-  },
-
-  typescript: {
-    extensions: ['ts', 'mts', 'cts'],
-    iconPath: './icons/typescript.svg'
-  },
-  'typescript-declaration': {
-    extensions: ['d.ts', 'd.mts', 'd.cts'],
-    iconPath: './icons/typescript-declaration.svg'
-  },
-  'typescript-test': {
-    extensions: ['test.ts', 'test.mts', 'test.cts', 'spec.ts', 'spec.mts', 'spec.cts'],
-    iconPath: './icons/typescript-test.svg'
-  },
-  'typescript-config': {
-    extensions: ['config.ts', 'config.mts', 'config.cts'],
-    iconPath: './icons/typescript-config.svg'
-  },
-
-  jsx: {
-    extensions: ['jsx'],
-    iconPath: './icons/jsx.svg'
-  },
-  'jsx-typescript': {
-    extensions: ['tsx'],
-    iconPath: './icons/tsx.svg'
-  },
-
-  vue: {
-    extensions: ['vue'],
-    iconPath: './icons/vue.svg'
-  },
   html: {
     extensions: ['html'],
     iconPath: './icons/html.svg'
@@ -93,11 +47,6 @@ export const iconDefinitions = {
   json: {
     extensions: ['json', 'jsonc'],
     iconPath: './icons/json.svg'
-  },
-  'json-config': {
-    names: ['tsconfig.json'],
-    extensions: ['config.json', 'config.jsonc'],
-    iconPath: './icons/json-config.svg'
   },
   markdown: {
     extensions: ['md', 'mdx', 'markdown'],
@@ -116,31 +65,6 @@ export const iconDefinitions = {
     extensions: ['yaml', 'yml'],
     iconPath: './icons/yaml.svg'
   },
-  rust: {
-    extensions: ['rs'],
-    iconPath: './icons/rust.svg'
-  },
-  zig: {
-    extensions: ['zig'],
-    iconPath: './icons/zig.svg'
-  },
-  shell: {
-    extensions: ['sh', 'bash', 'zsh', 'fish', 'bashrc', 'bash_profile', 'bash_aliases'],
-    iconPath: './icons/shell.svg'
-  },
-
-  npm: {
-    names: ['package.json', 'package-lock.json', 'pnpm-lock.yaml', 'yarn.lock'],
-    iconPath: './icons/npm.svg'
-  },
-  eslint: {
-    names: ['.eslintrc.*', 'eslint.*'],
-    iconPath: './icons/eslint.svg'
-  },
-  vite: {
-    extensions: ['vite.*'],
-    iconPath: './icons/vite.svg'
-  },
   env: {
     names: ['.env', '.env.*'],
     iconPath: './icons/env.svg'
@@ -157,8 +81,82 @@ export const iconDefinitions = {
     extensions: ['gitignore', 'gitattributes', 'gitmodules', 'gitkeep', 'gitconfig', 'gitignore_global'],
     iconPath: './icons/git.svg'
   },
+
+  javascript: {
+    extensions: ['js', 'mjs', 'cjs'],
+    iconPath: './icons/javascript.svg'
+  },
+  'javascript-test': {
+    extensions: ['test.js', 'test.mjs', 'test.cjs', 'spec.js', 'spec.mjs', 'spec.cjs'],
+    iconPath: './icons/javascript-test.svg'
+  },
+  jsx: {
+    extensions: ['jsx'],
+    iconPath: './icons/jsx.svg'
+  },
+
+  typescript: {
+    extensions: ['ts', 'mts', 'cts'],
+    iconPath: './icons/typescript.svg'
+  },
+  'typescript-test': {
+    extensions: ['test.ts', 'test.mts', 'test.cts', 'spec.ts', 'spec.mts', 'spec.cts'],
+    iconPath: './icons/typescript-test.svg'
+  },
+  'typescript-declaration': {
+    extensions: ['d.ts', 'd.mts', 'd.cts'],
+    iconPath: './icons/typescript-declaration.svg'
+  },
+  tsx: {
+    extensions: ['tsx'],
+    iconPath: './icons/tsx.svg'
+  },
+
+  config: {
+    names: ['tsconfig.json'],
+    iconPath: './icons/config.svg' 
+  },
+  'json-config': {
+    names: ['tsconfig.json'],
+    extensions: ['config.json', 'config.jsonc'],
+    iconPath: './icons/json-config.svg'
+  },
+  'javascript-config': {
+    extensions: ['config.js', 'config.mjs', 'config.cjs'],
+    iconPath: './icons/javascript-config.svg'
+  },
+  'typescript-config': {
+    extensions: ['config.ts', 'config.mts', 'config.cts'],
+    iconPath: './icons/typescript-config.svg'
+  },
+
+  vue: {
+    extensions: ['vue'],
+    iconPath: './icons/vue.svg'
+  },
+  rust: {
+    extensions: ['rs'],
+    iconPath: './icons/rust.svg'
+  },
+  shell: {
+    extensions: ['sh', 'bash', 'zsh', 'fish', 'bashrc', 'bash_profile', 'bash_aliases'],
+    iconPath: './icons/shell.svg'
+  },
+
+  npm: {
+    names: ['package.json', 'package-lock.json', 'pnpm-lock.yaml', 'yarn.lock'],
+    iconPath: './icons/npm.svg'
+  },
+  eslint: {
+    names: ['.eslintrc.*', 'eslint.*', 'eslint.config.*'],
+    iconPath: './icons/eslint.svg'
+  },
+  vite: {
+    extensions: ['vite.*'],
+    iconPath: './icons/vite.svg'
+  },
   docker: {
-    names: ['dockerfile', ".dockerignore", 'docker-compose.*', 'dockerfile.*'],
+    names: ['dockerfile', '.dockerignore', 'docker-compose.*', 'dockerfile.*'],
     iconPath: './icons/docker.svg'
   }
 } satisfies Record<string, IconDefinition & {
